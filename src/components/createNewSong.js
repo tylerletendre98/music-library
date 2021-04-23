04abfec145480a2b248b5eb7b80cdc6ddb3d20b3
T U <tylerletendre98@gmail.com>
2021-04-22 21:59:11 -0400

    added functionally to the form so you can add a song using the form and added a button to each row but doesnt work as of right now

--- src/components/createNewSong.js
+++ src/components/createNewSong.js
@@ -1,26 +1,76 @@
-import React from 'react';
+import axios from 'axios';
+import React, {Component} from 'react';
 import './createNewSong.css';
 
-function CreateSong(){
-    return(
-        <div className ="container">
-            <div className='form-head'>
-                <h3>Create a new song below</h3>
+class CreateSong extends Component{
+    state = {
+        title:'',
+        album:'',
+        artist:'',
+        genre:'',
+        releaseDate:'',
+    }
+
+    changeHandler= e =>{
+        this.setState({[e.target.name]: e.target.value})
+    }
+
+    submitHandler = e =>{
+        axios.post('http://localhost:5000/api/songs', this.state)
+            .then(response =>{
+                this.setState({response})
+            })
+            .catch(error => {
+                console.log(error)
+            })
+    }
+    render(){
+        return(
+            <div className ="container">
+                <div className='form-head'>
+                    <h4>Create a new song below</h4>
+                </div>
+                <div>
+                    <form action="create-new-song" onSubmit={this.submitHandler}>
+                        <label htmlFor="title">Title :</label>
+                            <input type="text" 
+                            name="title" 
+                            placeholder="enter title"
+                            value={this.title}
+                            onChange={this.changeHandler} 
+                            />
+                        <label htmlFor="album">Album :</label>
+                            <input type="text" 
+                            name="album" 
+                            placeholder="enter album"
+                            value={this.album} 
+                            onChange={this.changeHandler} 
+                            />
+                        <label htmlFor="artist">Artist :</label>
+                            <input type="text" 
+                             name="artist" 
+                             placeholder="enter artist" 
+                             value={this.artist}
+                             onChange={this.changeHandler}
+                             />
+                        <label htmlFor="genre">Genre :</label>
+                            <input type="text" 
+                            name="genre" 
+                            placeholder="enter genre"
+                            value={this.genre}
+                            onChange={this.changeHandler}/>
+                        <label htmlFor="release-date">Release date :</label>
+                            <input type="text" 
+                            name="releaseDate" 
+                            placeholder="enter Release date"
+                            value={this.releaseDate} 
+                            onChange={this.changeHandler}
+                            />
+                        <button type="submit">Submit</button>
+                    </form>
+                </div>
             </div>
-            <div>
-                <form action="create-new-song">
-                    <label htmlFor="title">Title :</label>
-                    <input type="text" placeholder="enter title here"/>
-                    <label htmlFor="album">Album :</label>
-                    <input type="text" placeholder="enter ablum here"/>
-                    <label htmlFor="artist">Artist :</label>
-                    <input type="text" placeholder="enter artist here"/>
-                    <label htmlFor="genre">Genre :</label>
-                    <input type="text" placeholder="enter genre here"/>
-                    <button>Submit</button>
-                </form>
-            </div>
-        </div>
-    )
+        )
+    }
 }
 export default CreateSong;
--- src/components/table.js
+++ src/components/table.js
@@ -1,34 +1,39 @@
 import React, {Component} from 'react';
 import './table.css'
+import axios from 'axios';
 
 class Table extends Component{
-    render(){
-        return (
-      <div>
-      <table className="table">
-            <thead>
-              <tr>
-                <th className="music-title">Title</th>
-                <th className="music-album">Album</th>
-                <th className="music-artist">Artist</th>
-                <th className="music-genre">Genre</th>
-                <th className="music-release-date">Release Date</th>
-              </tr>
-            </thead>
-            <tbody>
-              {this.props.songs.map((songs)=>(
-              <tr>
-                <td>{songs.title}</td>
-                <td>{songs.album}</td>
-                <td>{songs.artist}</td>
-                <td>{songs.genre}</td>
-                <td>{songs.releaseDate}</td>
-              </tr>))}
-            </tbody>
-          </table>
-      </div>
-        );
-    }
+  deleteRow(e){
+    axios.delete('http://localhost:5000/api/songs/1')
+  }
+  render(){
+      return (
+    <div>
+    <table className="table">
+          <thead>
+            <tr>
+              <th className="music-title">Title</th>
+              <th className="music-album">Album</th>
+              <th className="music-artist">Artist</th>
+              <th className="music-genre">Genre</th>
+              <th className="music-release-date">Release Date</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.props.songs.map((songs)=>(
+            <tr key={songs.id}>
+              <td>{songs.title}</td>
+              <td>{songs.album}</td>
+              <td>{songs.artist}</td>
+              <td>{songs.genre}</td>
+              <td>{songs.releaseDate}</td>
+              <button onClick={this.deleteRow}>Delete</button>
+            </tr>))}
+          </tbody>
+        </table>
+    </div>
+      );
+  }
 }
 
 
